a40b2c0b80327107c8e168e7f52a37cee3c9db6f
Cactus <ndementev56@gmail.com>
2026-03-11 14:42:35 +0200

    added iterator

--- 1geniter.js
+++ 1geniter.js
@@ -1,7 +1,17 @@
 function* fibgen() {
-    let a = 0n, b = 1n;
+    let a = 0, b = 1;
     while (true) {
         yield a;
         [a, b] = [b, a + b];
     }
+}
+
+const gen = fibgen();
+let total = 0n;
+
+for (let i = 0; i < 20; i++) {
+    const val = gen.next().value;
+    total += BigInt(val);
+    const avg = total / BigInt(i + 1);
+    console.log(`result=${val}    total=${total}  avg=${avg}`);
 }
